2e7958bc493faa4c07bbeb90b89b83e7e45d55ec
ibrohimov0 <ibrohimovabdulloh573@gmail.com>
2026-07-05 17:10:27 +0500

    customize theme colors and typography

--- app/src/main/java/uz/ibrohimov/calculator/ui/theme/Color.kt
+++ app/src/main/java/uz/ibrohimov/calculator/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package uz.ibrohimov.calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val WarmWhite = Color(0xFFF1EFE8)
+val PureWhite = Color(0xFFFFFFFF)
+val UtilityGray = Color(0xFFF8F7F4)
+val TextMuted = Color(0xFF888780)
+val TextPrimary = Color(0xFF0B0B0B)
+val AccentBlue = Color(0xFF378ADD)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBg = Color(0xFF1A1A18)
+val DarkSurface = Color(0xFF2C2C2A)
+val DarkUtility = Color(0xFF252523)
+val DarkTextPrimary = Color(0xFFF1EFE8)
